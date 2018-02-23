971c36a7d7591034f58d451423b3ff190354ed35
Vijay Thangavel <vijay.php@gmail.com>
2018-02-23 14:56:53 +0530

    Added react modules.

---
+++ redux_vijay/app/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+const App = () => (
+    <h2>Vijay Thangavel's app</h2>
+);
+
+render( <App /> , document.getElementById('root'))
---
+++ redux_vijay/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+    <h2>My Redux App</h2>,
+    document.getElementById('root')
+);
